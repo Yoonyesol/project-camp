3e8100e94e51b8d378b4561a3a25851271541cb3
Yoonyesol <maniac970430@naver.com>
2024-06-05 10:32:13 +0900

    todo server 작성

---
+++ server/server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.json());
+
+let todos = [];
+
+app.get("/", (req, res) => {
+  res.send("Hello Snifer!");
+});
+
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
+app.post("/todos", (req, res) => {
+  const { text } = req.body;
+  const newTodo = {
+    id: uuidv4(),
+    text,
+    completed: false,
+  };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const { id } = req.params;
+  todos = todos.filter((todo) => todo.id !== id);
+  res.status(204).send();
+});
+
+app.patch("/todos/:id", (req, res) => {
+  const { id } = req.params;
+  todos = todos.map((todo) =>
+    todo.id === id ? { ...todo, completed: !todo.completed } : todo
+  );
+  res.json(todos.filter((todo) => todo.id === id));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
